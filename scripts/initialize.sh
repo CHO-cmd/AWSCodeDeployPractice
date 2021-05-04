7667824f701c7a183acae6c09d0c2ec4d0d76012
CHO-cmd <wjdqls9362@gmail.com>
2021-05-04 17:09:48 +0900

    test6

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd /home/ubuntu/AWSCodeDeployPractice
 npm install
-npm install pm2 -g
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/AWSCodeDeployPractice
+npm install pm2 -g
 pm2 stop app.js
