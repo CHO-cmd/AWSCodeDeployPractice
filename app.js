afb073177b69a27eeca7d3a7412fb2674499fe1a
CHO-cmd <wjdqls9362@gmail.com>
2021-05-04 18:22:16 +0900

    please

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const PORT = 3000;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).json({ data: "오설록 먹으러 갸자" });
+  res.status(200).json({ data: "osuloc" });
 });
 app.listen(PORT, () => {
   console.log("3000포트를 이용한 서버 실행");
